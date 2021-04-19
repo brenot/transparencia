c53c0dc89e095b4a563437b17d7c596b9ea14ec4
Breno Trengrouse <brenot@gmail.com>
2021-04-19 16:02:05 -0300

    Bootstrap Migration 019 - new header

--- resources/views/templates/superior.blade.php
+++ resources/views/templates/superior.blade.php
@@ -1,3 +1,72 @@
+
+<div class="novo-header">
+
+    <div class="container novo-header">
+        <header class="blog-header pt-3">
+            <div class="row flex-nowrap justify-content-between align-items-center">
+                <div class="col-6 col-md-4 col-lg-2 pt-1">
+                    <a href="http://www.alerj.rj.gov.br/">
+                        <img src="/images/ALERJ_NOVO_horizontal-azul-01.png" class="img-fluid"  alt="Assembleia Legislativa do Estado do Rio de Janeiro" title="Assembleia Legislativa do Estado do Rio de Janeiro">
+                    </a>
+                </div>
+
+
+                <div class="col-6 col-md-4 d-flex justify-content-end align-items-center">
+                    <div class="redes_sociais">
+                        <div class="logotipo_redes">
+                            <a target="_blank" href="https://instagram.com/instalerj/" title="Instagram">
+                                <img src="/images/icons/icone_36.png" width="20" height="20" alt="Instagram" title="Instagram">
+                            </a>
+                        </div>
+
+                        <div class="logotipo_redes">
+                            <a target="_blank" href="https://www.facebook.com/assembleiaRJ" title="Facebook">
+                                <img src="/images/icons/icone_06.png" width="20" height="20" alt="Facebook" title="Facebook">
+                            </a>
+                        </div>
+
+                        <div class="logotipo_redes">
+                            <a target="_blank" href="https://twitter.com/alerj" title="Twitter">
+                                <img src="/images/icons/icone_08.png" width="20" height="20" alt="Twitter" title="Twitter">
+                            </a>
+                        </div>
+
+                        <div class="logotipo_redes">
+                            <a target="_blank" href="https://www.youtube.com/user/dcsalerj" title="Youtube">
+                                <img src="/images/icons/icone_09.png" width="20" height="20" alt="Youtube" title="Youtube">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </header>
+
+
+{{--
+        <div class="nav-scroller py-1 mb-2">
+            <nav class="nav d-flex justify-content-between">
+                <a class="p-2 text-muted" href="#">World</a>
+                <a class="p-2 text-muted" href="#">U.S.</a>
+                <a class="p-2 text-muted" href="#">Technology</a>
+                <a class="p-2 text-muted" href="#">Design</a>
+                <a class="p-2 text-muted" href="#">Culture</a>
+                <a class="p-2 text-muted" href="#">Business</a>
+                <a class="p-2 text-muted" href="#">Politics</a>
+                <a class="p-2 text-muted" href="#">Opinion</a>
+                <a class="p-2 text-muted" href="#">Science</a>
+                <a class="p-2 text-muted" href="#">Health</a>
+                <a class="p-2 text-muted" href="#">Style</a>
+                <a class="p-2 text-muted" href="#">Travel</a>
+            </nav>
+        </div>
+--}}
+
+    </div>
+</div>
+
+
+{{--
 <div class="superior">
     <div class="brilho ">
         <div class="conteudo">
@@ -37,3 +106,5 @@
         </div>
     </div>
 </div>
+
+--}}
